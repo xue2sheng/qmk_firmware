b8d1da4a11716189b5928384d3c173e9cb4d70df
Viet Pham <viet@Viets-MacBook-Pro.local>
2020-02-01 12:55:57 -0500

    update keymap

--- keyboards/kyria/keymaps/littlekeyboards/keymap.c
+++ keyboards/kyria/keymaps/littlekeyboards/keymap.c
@@ -15,6 +15,9 @@
  */
 #include QMK_KEYBOARD_H
 
+
+
+
 enum layers {
     _QWERTY = 0,
     _NUMBERS,
@@ -48,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       KC_CMES,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   ______, ______,    ______, ______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-                        KC_ENA ,KC_LALT, NAV, NUMBERS,  ______,                    NAV,    KC_SPC,  CODE,    KC_RGUI, KC_DEL
+      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_TAB, KC_TAB,    KC_DEL, KC_DEL, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+                        KC_ENA ,KC_LALT, NAV, NUMBERS,  KC_ENT,                    NAV,    KC_SPC,  CODE,    KC_RGUI, KC_DEL
     ),
 
 /*
